e1dfebc5ab1062a0f33c550bba03224d7a453ef8
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-08 17:49:43 +0000

    Cleaner formatter stubs

--- data/fileformats/reactions/formats/format_null.py
+++ data/fileformats/reactions/formats/format_null.py
@@ -1,11 +1,12 @@
-import system
-import reaction
-import species
+from system import System
+from reaction import Reaction
+from species import Species
 
 class Formatter:
     def handles(self):
         return ["null"]
     def parse(self,file):
-        return system.System()
+        system=System()
+        return system
     def write(self,file,system):
         pass
