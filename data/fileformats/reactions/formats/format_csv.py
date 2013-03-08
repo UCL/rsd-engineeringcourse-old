d467c9d8aa512f665bbd3e6292440c4fec85dfcd
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-08 18:01:22 +0000

    Parse CSV files

--- data/fileformats/reactions/formats/format_csv.py
+++ data/fileformats/reactions/formats/format_csv.py
@@ -1,15 +1,26 @@
-import CSV
+import csv
 
 from system import System
 from reaction import Reaction
 from species import Species
 
-
 class Formatter:
     def handles(self):
         return ["csv"]
     def parse(self,file):
-        reader=csv.reader(file)
         system=System()
+        reader=csv.reader(file)
+        for row in reader:
+            reverse=float(row.pop())
+            forward=float(row.pop()) 
+            products=[]
+            reactants=[] 
+            current=row.pop()
+            while current != "-":
+                products.append(current)
+                current=row.pop()
+            reactants=row
+            system.add(reactants,products,forward,reverse)
+        return system
     def write(self,file,system):
         pass
