4da8c3d05e5ed40f391436ebbc069955bd21f2cf
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-10 13:25:13 +0000

    Json solution

--- data/fileformats/reactions/formats/format_json.py
+++ data/fileformats/reactions/formats/format_json.py
@@ -2,12 +2,28 @@ from system import System
 from reaction import Reaction
 from species import Species
 from base_formatter import BaseFormatter
-
+import json
 
 class Formatter(BaseFormatter):
     handles=["json"]
     def parse(self,file):
         system=System()
+        
+        model = json.load(file)
+        for reaction in model:
+            system.add(reaction.get("reactants"),reaction.get("products"),reaction["rate"])
+
         return system
+
     def write(self,file,system):
-        pass  
+        # Make a model of the system just as dicts and lists:
+        model=[dict(
+            rate=reaction.rate,
+            reactants=[reactant.label for reactant in reaction.reactants],
+            products=[product.label for product in reaction.products]
+        ) for reaction in system.reactions
+        ]
+
+        # Dump as json
+
+        json.dump(model,file)  
