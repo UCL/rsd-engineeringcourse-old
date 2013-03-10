cf3de11837d6db5f07f0d81861495aa9ecbe87ae
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-10 14:19:10 +0000

    Add extra test

--- data/fileformats/reactions/test/base_system1.py
+++ data/fileformats/reactions/test/base_system1.py
@@ -39,9 +39,11 @@ class BaseSystem1Test(unittest.TestCase):
         system2=self.write_then_read(system)
         self.assertItemsEqual(system2.species,["A", "B", "C","D","E"])
         self.assertEqual(len(system2.reactions),4)
+        self.assertItemsEqual([x.rate for x in system2.reactions],[0.3,0.2,0.3,0.1])
         
     def test_read_write_read(self):
         system=self.parse_input()
         system2=self.write_then_read(system)
         self.assertItemsEqual(system2.species,["A", "B", "C","D","E"])
         self.assertEqual(len(system2.reactions),4)
+        self.assertItemsEqual([x.rate for x in system2.reactions],[0.3,0.2,0.3,0.1])
