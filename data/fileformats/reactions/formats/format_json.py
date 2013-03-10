30a4967ea04f9b0b9e019736fa4bc37d7fe93d48
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-10 13:30:21 +0000

    Stubs for json and yaml

--- data/fileformats/reactions/formats/format_json.py
+++ data/fileformats/reactions/formats/format_json.py
@@ -2,12 +2,23 @@ from system import System
 from reaction import Reaction
 from species import Species
 from base_formatter import BaseFormatter
-
+import json
 
 class Formatter(BaseFormatter):
     handles=["json"]
     def parse(self,file):
         system=System()
+        
+        model = json.load(file)
+
+        # Add reactions according to the json model
+        
         return system
+
     def write(self,file,system):
-        pass  
+        # Make a model of the system just as dicts and lists:
+        model=None # Put your solution here
+
+        # Dump as json
+
+        json.dump(model,file) 
--- data/fileformats/reactions/formats/format_yaml.py
+++ data/fileformats/reactions/formats/format_yaml.py
@@ -2,12 +2,23 @@ from system import System
 from reaction import Reaction
 from species import Species
 from base_formatter import BaseFormatter
-
+import yaml
 
 class Formatter(BaseFormatter):
     handles= ["yaml", "yml"]
     def parse(self,file):
         system=System()
+        
+        model = yaml.load(file)
+                   
+        # Add reactions according to the yaml model
+
         return system
+
     def write(self,file,system):
-        pass
+        # Make a model of the system just as dicts and lists:
+        model=None # Put your solution here
+
+        # Dump as yaml
+
+        yaml.dump(model,file)
