521e774dd482ad5276bb1a35f539dce528a7eefd
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-10 11:24:49 +0000

    Parser solution for xml

--- data/fileformats/reactions/formats/format_xml.py
+++ data/fileformats/reactions/formats/format_xml.py
@@ -21,8 +21,10 @@ class Formatter:
 
         self.tree = etree.ElementTree(self.root)
         
-    def queryXpath(self,query):
-        return self.tree.xpath(query)
+    def queryXpath(self,query,start=None):
+        if not start:
+            start=self.tree
+        return start.xpath(query)
 
     def handles(self):
         return ["xml"]
@@ -32,6 +34,14 @@ class Formatter:
         system=System() 
         
         # put your solution here
+        
+        reactions=self.queryXpath("//reaction")
+        for reaction in reactions:
+            products=self.queryXpath("product/@label",reaction)
+            reactants=self.queryXpath("reactant/@label",reaction)
+            rate=float(self.queryXpath("@rate",reaction)[0])
+            
+            system.add(reactants,products,rate)
         
         return system
 
