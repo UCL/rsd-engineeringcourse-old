b8b21bf42c31bf3280e4f326fd74c05f0488f3fe
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-10 14:19:00 +0000

    XDR solution

--- data/fileformats/reactions/formats/format_xdr.py
+++ data/fileformats/reactions/formats/format_xdr.py
@@ -19,7 +19,19 @@ class Formatter(BaseFormatter):
         assert version==Formatter.version
         system=System() 
         
-        # Your XDR solution here
+        reaction_count=unpacker.unpack_uint()
+        for reaction_index in range(reaction_count):
+            rate=unpacker.unpack_double()
+            reactant_count=unpacker.unpack_uint()
+            reactants=[""]*reactant_count
+            for reactant_index in range(reactant_count):
+                reactants[reactant_index]=unpacker.unpack_string()
+            product_count=unpacker.unpack_uint()
+            products=[""]*product_count
+            for product_index in range(product_count):
+                products[product_index]=unpacker.unpack_string()
+
+            system.add(reactants,products,rate)
         
  
         return system
@@ -27,8 +39,14 @@ class Formatter(BaseFormatter):
         packer=xdrlib.Packer() 
         packer.pack_uint(Formatter.magic)
         packer.pack_uint(Formatter.version)
-
-        # Your XDR solution here
-
+        packer.pack_uint(len(system.reactions))
+        for reaction in system.reactions:
+            packer.pack_double(reaction.rate)
+            packer.pack_uint(len(reaction.reactants))
+            for reactant in reaction.reactants:
+                packer.pack_string(reactant.label)
+            packer.pack_uint(len(reaction.products))
+            for product in reaction.products:
+                packer.pack_string(reactant.label)
         file.write(packer.get_buffer())
              
