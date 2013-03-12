72c85d9f02d0863d3d2ea854d7de11e8ccba2878
James Hetherington <j.hetherington@ucl.ac.uk>
2013-03-12 14:46:01 +0000

    Relative import in formatters

--- data/fileformats/reactions/formats/format_xml.py
+++ data/fileformats/reactions/formats/format_xml.py
@@ -2,9 +2,9 @@ from lxml import etree
 from lxml.builder import E as builder
 import mako.template as mk 
 
-from reaction_system import System
-from reaction import Reaction
-from species import Species
+from ..reaction_system import System
+from ..reaction import Reaction
+from ..species import Species
 from base_formatter import BaseFormatter
 
 import os
